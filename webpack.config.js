176e2a55d86cca2651103129f2ab1e34f45ee6f0
drmyersii <davidrmyersii@gmail.com>
2016-08-18 13:19:47 -0400

    Implementing plugin for Babel@5 styled module bundling (normal require).

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ module.exports = {
     entry: './src/dictionary.js',
     output: {
         path: './dist',
-        filename: 'dictionary.js'
+        filename: 'dictionary.js',
+        libraryTarget: 'commonjs2'
     },
     module: {
         loaders: [
@@ -11,6 +12,9 @@ module.exports = {
                 loader: 'babel-loader',
                 query: {
                   presets: 'es2015',
+                  plugins: [
+                      'add-module-exports'
+                  ]
                 }
             }
         ]
